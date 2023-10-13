33490cf93c6de3e27cedeb62000e24d2eebea4b6
dsunjic <114637883+DylanSunj@users.noreply.github.com>
2023-10-13 20:06:52 +0000

    Added links and general headers

--- app.rb
+++ app.rb
@@ -2,8 +2,53 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
 end
+
+get("/rock") do 
+  options = ["rock", "paper", "scissors"]
+
+  @comp_choice = options.sample 
+
+  if @comp_choice == "rock"
+    @decision = "We tied!"
+  elsif @comp_choice == "paper"
+    @decision = "We lost!"
+  elsif @comp_choice == "scissors"
+    @decision = "We won!"
+  end
+
+  erb(:rock)
+end 
+
+get("/paper") do 
+  options = ["rock", "paper", "scissors"]
+
+  @comp_choice = options.sample 
+
+  if @comp_choice == "rock"
+    @decision = "We won!"
+  elsif @comp_choice == "paper"
+    @decision = "We tied!"
+  elsif @comp_choice == "scissors"
+    @decision = "We lost!"
+  end
+
+  erb(:paper)
+end 
+
+get("/scissors") do 
+  options = ["rock", "paper", "scissors"]
+
+  @comp_choice = options.sample 
+
+  if @comp_choice == "rock"
+    @decision = "We lost!"
+  elsif @comp_choice == "paper"
+    @decision = "We won!"
+  elsif @comp_choice == "scissors"
+    @decision = "We tied!"
+  end
+
+  erb(:scissors)
+end 
